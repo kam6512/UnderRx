6eb826233453e3391a73703edce9e0de104a949c
kam6512 <kam6512@gmail.com>
2018-03-21 15:36:50 +0900

    add diff

--- src/main/java/com/kam6512/underrx/UnderArrayRx.java
+++ src/main/java/com/kam6512/underrx/UnderArrayRx.java
@@ -1,2 +1,111 @@
+package com.kam6512.underrx;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+import io.reactivex.Emitter;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * UnderRx is a java port of "Array" in Lodash
+ *
+ * @author kam6512
+ */
 public class UnderArrayRx {
+
+    public Observable<List<? extends List<?>>> chunk(List<?> list, int size) {
+        return Observable.create(emitter -> {
+            if (checkList(list)) {
+                emitter.onNext(Lists.newArrayList());
+                emitter.onComplete();
+                return;
+            }
+            emitter.onNext(Lists.partition(list, size));
+            emitter.onComplete();
+        });
+    }
+
+    public Observable<List<Integer>> compactInteger(List<Integer> list) {
+        return Observable.create(emitter -> {
+            if (checkList(list)) {
+                emitter.onNext(Lists.newArrayList());
+                emitter.onComplete();
+                return;
+            }
+            ArrayList<Integer> result = Lists.newArrayList(list);
+            result.removeIf(item -> item == 0);
+            emitter.onNext(result);
+            emitter.onComplete();
+        });
+    }
+
+    public Observable<List<Float>> compactFloat(List<Float> list) {
+        return Observable.create(emitter -> {
+            if (checkList(list)) {
+                emitter.onNext(Lists.newArrayList());
+                emitter.onComplete();
+                return;
+            }
+            ArrayList<Float> result = Lists.newArrayList(list);
+            result.removeIf(item -> item == 0f);
+            emitter.onNext(result);
+            emitter.onComplete();
+        });
+    }
+
+    public Observable<List<String>> compactString(List<String> list) {
+        return Observable.create(emitter -> {
+            if (checkList(list)) {
+                emitter.onNext(Lists.newArrayList());
+                emitter.onComplete();
+                return;
+            }
+            ArrayList<String> result = Lists.newArrayList(list);
+            result.removeIf(Strings::isNullOrEmpty);
+            emitter.onNext(result);
+            emitter.onComplete();
+        });
+    }
+
+    // concat is passed -> use add or addAll
+
+    public Observable<List<Integer>> diff(List<Integer> diffList, List<Integer> filterList) {
+        return Observable.create(emitter -> {
+            if (checkList(diffList) || checkList(filterList)) {
+                emitter.onNext(Lists.newArrayList());
+                emitter.onComplete();
+                return;
+            }
+            ArrayList<Integer> result = Lists.newArrayList(diffList);
+            result.removeAll(filterList);
+            emitter.onNext(result);
+            emitter.onComplete();
+        });
+    }
+
+    public Observable<List<?>> drop(List<?> list, int count) {
+        return Observable.create(emitter -> {
+            if (checkList(list)) {
+                emitter.onNext(Lists.newArrayList());
+                emitter.onComplete();
+                return;
+            }
+            ArrayList<?> result = Lists.newArrayList(list);
+            int subLength = list.size() - count;
+            result.subList(0, subLength);
+            emitter.onNext(result);
+            emitter.onComplete();
+        });
+    }
+
+
+    private static boolean checkList(List list) {
+        return (list == null || list.isEmpty());
+    }
 }
--- src/test/java/com/kam6512/underrx/UnderArrayRxTest.java
+++ src/test/java/com/kam6512/underrx/UnderArrayRxTest.java
@@ -1,5 +1,32 @@
-import static org.junit.Assert.*;
+package com.kam6512.underrx;
+
+import org.junit.Test;
+
+import java.util.Arrays;
 
 public class UnderArrayRxTest {
 
+    private UnderArrayRx test = new UnderArrayRx();
+
+    @Test
+    public void chunk() {
+        test.chunk(Arrays.asList(1, 2, 3, 4, 5, 6, 7), 3)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void compactInteger() {
+        test.compactInteger(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 0, -1))
+                .subscribe(System.out::println, throwable -> {
+                    System.out.println(throwable.toString());
+                });
+    }
+
+    @Test
+    public void compactString() {
+        test.compactString(Arrays.asList("a", "b", "c", ""))
+                .subscribe(System.out::println, throwable -> {
+                    System.out.println(throwable.toString());
+                });
+    }
 }
